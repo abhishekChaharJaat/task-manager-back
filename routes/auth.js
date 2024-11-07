09b086eefb3072ffd1f01491478fabd95f149ebb
email <abhishskchahar200@gmail.com>
2024-11-08 01:22:26 +0530

    fourth

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const fetchUser = require("../middleware/fetchUser");
+const fetchUser = require("../middleware/fetchuser");
 
 const JWT_SECRET = "iamjaat";
 
